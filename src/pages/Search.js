256ecb55136f562568cdefbeb3c7edc93b7d40d8
juno <juno7803@naver.com>
2021-05-15 17:35:19 +0900

    feat: add footer & header

---
+++ src/pages/Search.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Search = () => {
+    return (
+        <SearchWrap>
+        </SearchWrap>
+    )
+}
+
+export default Search;
+
+const SearchWrap = styled.div`
+
+`

005e827a06728bf5f6a66736080151b83b089ee1
Juno <juno7803@naver.com>
2021-05-20 03:40:13 +0900

    feat: ScrollSlider 분리 (#5)
    * chore: eslint setting airbnb로 변경
    
    * feat: ScrollSlider components 분리
    
    * docs: README eslint 추가

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,35 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
-import {
-  Banner,
-  SliderOne,
-  SliderTwo,
-  SliderThree,
-  SliderFour,
-  SliderFifth,
-  SliderSixth,
-  SliderSeventh,
-} from '../assets';
+import { Banner } from '../assets';
+import { ScrollSlider } from '../components';
 
 const MainPage = () => {
   return (
     <MainWrap>
       <MainContainer>
         <FourthContainer>
-          <img className="banner" src={Banner} />
-          <div className="slider">
-            <div className="slider__title">RECOMMENDED ARTICLES</div>
-            <div className="slider__sub">브런치의 다양한 글을 만나보세요.</div>
-            <div className="slider__content">
-              <img className="slider__content--img" src={SliderOne} />
-              <img className="slider__content--img" src={SliderTwo} />
-              <img className="slider__content--img" src={SliderThree} />
-              <img className="slider__content--img" src={SliderFour} />
-              <img className="slider__content--img" src={SliderFifth} />
-              <img className="slider__content--img" src={SliderSixth} />
-              <img className="slider__content--img" src={SliderSeventh} />
-            </div>
-          </div>
+          <img className="banner" src={Banner} alt="banner" />
+          <ScrollSlider />
         </FourthContainer>
       </MainContainer>
     </MainWrap>
@@ -56,34 +36,5 @@ const FourthContainer = styled.div`
   align-items: center;
   .banner {
     margin-top: 15.5rem;
-  }
-  .slider {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 14.6rem;
-    &__title {
-      letter-spacing: 0.4rem;
-      font-size: 1.8rem;
-    }
-    &__sub {
-      margin-top: 0.8rem;
-      color: #bdbdbd;
-      font-size: 1.2rem;
-      letter-spacing: -0.03rem;
-    }
-    &__content {
-      width: 96rem;
-      height: 68rem;
-      margin-top: 4.4rem;
-      display: flex;
-      align-items: flex-start;
-      flex-wrap: nowrap;
-      overflow-x: auto;
-      margin-bottom: 10rem;
-      &--img {
-        margin-left: 2rem;
-      }
-    }
   }
 `;

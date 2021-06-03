df85aa97ca551a4ddac991f713eb89e7a1eb9cd0
juno <juno7803@naver.com>
2021-06-03 17:19:12 +0900

    feat: articles 서버 연결 및 파일 이름 변경

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ export { default as Header } from './common/Header';
 export { default as Footer } from './common/Footer';
 export { default as Title } from './main/Title';
 export { default as Popup } from './main/Popup';
-export { default as ScrollSlider } from './main/ScrollSlider';
+export { default as Articles } from './main/Articles';
 export { default as Writers } from './main/Writers';
 export { default as Keywords } from './main/Keywords';
 export { default as MainSlider } from './main/MainSlider';
